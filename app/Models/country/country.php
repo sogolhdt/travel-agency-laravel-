88c1200455761d949d540d11b1d4a551eca7de3f
sogol hedayati <66595003+sogolhdt@users.noreply.github.com>
2024-02-11 15:30:12 +0330

    update: login and register page redesigned

---
+++ app/Models/country/country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\country;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class country extends Model
+{
+    use HasFactory;
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,56 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
+    <div class="reservation-form">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-12">
+                    <form id="reservation-form" method="POST" role="search" action="{{ route('login') }}">
+                        @csrf
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h4>Login</h4>
+                            </div>
+                            <div class="col-md-12">
+                                <fieldset>
+                                    <label for="Name" class="form-label">Your Email</label>
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" placeholder="email"
+                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </fieldset>
+                            </div>
+
+                            <div class="col-md-12">
+                                <fieldset>
+                                    <label for="Name" class="form-label">Your Password</label>
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        required autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </fieldset>
+                            </div>
+                            <div class="col-lg-12">
+                                <fieldset>
+                                    <button type="submit" class="main-button">login</button>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
